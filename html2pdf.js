d37861146eaa89f00f1db7ba6e072d893ef7d287
Albert Sancho <asamo7@gmail.com>
2018-12-26 22:26:00 +0100

    New version

--- html2pdf.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var pdf = require('html-pdf');
-var html = fs.readFileSync('./public/index.html', 'utf8');
-var options = { base: './static' };
-
-pdf.create(html, options).toFile('./public/cv.pdf', function(err, res) {
-  if (err) return console.log(err);
-  console.log(res); // { filename: '/app/businesscard.pdf' }
-});
